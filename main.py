d421b899b691ee76a9cda90d2de6c4c55cbf0ba2
Himanshu dua <hdua@q3tech.com>
2017-04-28 12:09:49 +0530

    Predict answe

--- main.py
+++ main.py
@@ -12,4 +12,23 @@ def get_data_from_csv(input_file):
         questionnaire_reader = csv.reader(csvfile)
         return [row for row in questionnaire_reader]
 
-print(get_all_questions_answer())
+
+def pre_process_data(data):
+    lst_questions = [row[0] for row in data]
+    dic_questionnaire = dict(data)
+    return lst_questions, dic_questionnaire
+
+
+def pridict_answer(query, limit=3, min_confidence=70):
+    lst_questions, dic_questionnaire = pre_process_data(
+        get_all_questions_answer())
+    from fuzzywuzzy import process
+    predicted_questions = process.extract(query,
+                                          lst_questions, limit=limit)
+    pridicted = []
+    for item in predicted_questions:
+        if min_confidence <= item[1]:
+            pridicted.append((item[0], item[1], dic_questionnaire[item[0]]))
+    print(pridicted)
+
+pridict_answer('Could I offer u $53.56 for it? It s all I got!')
